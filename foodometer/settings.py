0c1ca4336b8d7701dc699e45cdde58562bb865ad
Kush Gogia <gogiakush@gmail.com>
2021-09-23 23:32:01 -0400

    commit

--- foodometer/settings.py
+++ foodometer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!tx@ul6$2$t&#v*2%vshcss#)y8f+_tya=%orit7vqol0a7zvi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
